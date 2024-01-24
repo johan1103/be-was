4e90ceefe829010ed7ee1975dbda7cc7e43ee97d
johan1103 <johan1103@naver.com>
2024-01-24 11:39:38 +0900

    feat: interceptor 구현체 주입

--- src/main/java/webserver/interceptor/InterceptorExecutionChain.java
+++ src/main/java/webserver/interceptor/InterceptorExecutionChain.java
@@ -3,10 +3,14 @@ package webserver.interceptor;
 import webserver.MyHttpServletRequest;
 import webserver.MyHttpServletResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InterceptorExecutionChain {
-  List<Interceptor> interceptors;
+  private final List<Interceptor> interceptors = new ArrayList<>();
+  public InterceptorExecutionChain(){
+    interceptors.add(new LoginUserValidateInterceptor());
+  }
   public boolean applyPreHandle(MyHttpServletRequest request){
     boolean successPreHandling = true;
     for(Interceptor i : interceptors){
